c01f670da1fe4bceb6168427e8558571f9bb6e5a
DragonXDev <amaardevx@gmail.com>
2024-05-09 10:14:23 -0700

    AdaDelta Optimizer Implementation

--- src/train_model.py
+++ src/train_model.py
@@ -4,7 +4,7 @@ from torch.nn import CrossEntropyLoss
 
 # Instance of NN, calculate cost, optimize
 classifier = DigitClassifier().to('cuda')
-optimizer = optim.Adam(classifier.parameters(), lr=1e-3)
+optimizer = optim.Adadelta(classifier.parameters(),lr=1e-3)
 cost_fn = CrossEntropyLoss()
 
 # Training Model
@@ -12,10 +12,9 @@ if __name__ == "__main__":
     for epoch in range(10):
         for batch in dataset:  # 32 chunked batches
             X, y = batch
-            X, y = X.to('cuda'), y.to('cuda')
+            X, y = X.to('cuda'), y.to('cuda') 
             yhat = classifier(X)
             cost = cost_fn(yhat, y)
-
             # Apply backpropagation
             optimizer.zero_grad()
             cost.backward()
